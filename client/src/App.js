cad0739c15a729e13b724810339f5d0097372a15
MobiusXXF <104108778+MobiusXXF@users.noreply.github.com>
2023-06-07 01:15:53 +0100

    Added first gym class card, more to be added and additional UI to implement.

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+      <div id="bjj" class="card">
+        <h2>Brazilian Jiu-Jitsu</h2>
+        <img src="./src/images/bjj.jpeg" alt="Brazilian Jiu-Jitsu" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Class Description
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      </div>
     </div>
   );
 }
 
 export default App;
+
