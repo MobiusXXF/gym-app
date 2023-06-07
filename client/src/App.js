6554a8e10fc87b83319a1fed56e1e40598cf3acf
MobiusXXF <104108778+MobiusXXF@users.noreply.github.com>
2023-06-07 02:02:50 +0100

    Added more cards, as well as images to use. Need to figure out correct path to add images to cards.

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,47 @@
 import './App.css';
+import './images/bjj.jpeg';
 
 function App() {
   return (
-    <div className="App">
-      <div id="bjj" class="card">
-        <h2>Brazilian Jiu-Jitsu</h2>
-        <img src="./src/images/bjj.jpeg" alt="Brazilian Jiu-Jitsu" />
+    <body>
+      <div className="App">
+        <header id="home">
+        <h1>Hercules Gym</h1>
         <p>
-          Class Description
+          Hover over the classes below and see what they have to offer!
         </p>
+      </header>
+      <hr/>
+        <div id="weights" class="card">
+          <h2>Weight Lifting</h2>
+          <img src="" alt="" />
+          <p>
+            Class Description
+          </p>
+        </div>
+        <div id="spin" class="card">
+          <h2>Spin Class</h2>
+          <img src="" alt="" />
+          <p>
+            Class Description
+          </p>
+        </div>
+        <div id="yoga" class="card">
+          <h2>Yoga</h2>
+          <img src="" alt="" />
+          <p>
+            Class Description
+          </p>
+        </div>
+        <div id="bjj" class="card">
+          <h2>Brazilian Jiu-Jitsu</h2>
+          <img src="./images/bjj.jpeg" alt="" />
+          <p>
+            Class Description
+          </p>
+        </div>
       </div>
-    </div>
+    </body>
   );
 }
 
